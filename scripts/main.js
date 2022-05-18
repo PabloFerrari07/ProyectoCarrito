a904059fc6406d89c374604f326c53bdc6be56c7
PabloFerrari <pablo.ferrari019@gmail.com>
2022-05-18 11:33:56 -0300

    iniciando

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const carrito = document.getElementById('carrito');
+const contenidoCarrito = document.querySelector('#cargarCarrito tbody');
+const vaciarCarrito = document.getElementById('vaciar_carrito');
+const listaCurso = document.getElementById('lista_carrito');
+let articuloCarrito = [];
